9fda1e8a52a83b8683137ccf609cd362286f7be9
Keshav <keshav950personal@gmail.com>
2025-06-05 13:58:16 +0530

    format

--- 07other-name-algo/next-permutation.cpp
+++ 07other-name-algo/next-permutation.cpp
@@ -4,15 +4,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-int main()
-{
+
+int main() {
     return 0;
 }
-class Solution
-{
+
+class Solution {
 public:
-    void nextPermutation(vector<int> &nums)
-    {
+    void nextPermutation(vector<int> &nums) {
         // Step 1 : Find the rightmost index i for which nums[i] < nums[i+1]
         // Step 2 : find the next greater element to nums[i] in the right side at some index j
         // Step 3 : swap i and j indexed elements
@@ -21,20 +20,18 @@ public:
         int n = nums.size();
         int i = n - 2; // not checking for n - 1 index since it doesn't has anything in it's right
 
-        while (i >= 0 && nums[i] >= nums[i + 1])
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
             i--;
+        }
 
-        if (i < 0)
-        {
+        if (i < 0) {
             reverse(nums.begin(), nums.end());
             return;
         }
 
         int j = n - 1, val = INT_MAX, valIdx = -1;
-        while (j > i)
-        {
-            if (nums[j] > nums[i] && nums[j] < val)
-            {
+        while (j > i) {
+            if (nums[j] > nums[i] && nums[j] < val) {
                 valIdx = j;
                 val = nums[j];
             }
